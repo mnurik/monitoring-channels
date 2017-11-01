bd19dbee24f6f6765ef72bdc29d9f4f7fee53776
Nurlan <nurlanmirzay@gmail.com>
2017-11-01 12:01:12 +0400

    give files from frontend branch

--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import channels from "./channels";
-import current from "./current";
-import control from "./control";
-
-export default combineReducers({ channels, current, control })
